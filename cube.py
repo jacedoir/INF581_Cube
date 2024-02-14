708dfcc89ab4e44e4838f4989b76b326bd6c76c7
jacedoir <remi.grz@free.fr>
2024-02-14 21:38:06 +0100

    Update cube.py

--- cube.py
+++ cube.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection,Line3DCollection
 
 
 
@@ -37,23 +37,25 @@ class Cube:
             row: int, the row to twist
             direction: int, 1 for clockwise, -1 for counterclockwise
         """
+        new_state = np.copy(self.state)
         try:
             assert row < self.size
             if direction == 1: # clockwise
-                self.sate[1][row], self.sate[2][row], self.state[3][row], self.state[4][row] = self.state[2][row],self.state[3][row],self.state[4][row],self.state[1][row]
+                new_state[1][row], new_state[2][row], new_state[3][row], new_state[4][row] = self.state[2][row],self.state[3][row],self.state[4][row],self.state[1][row]
             else: # counterclockwise
-                self.cube[1][row], self.cube[2][row], self.cube[3][row], self.cube[4][row] = self.cube[4][row],self.cube[1][row], self.cube[2][row], self.cube[3][row]
+                new_state[1][row], new_state[2][row], new_state[3][row], new_state[4][row] = self.state[4][row],self.state[1][row],self.state[2][row],self.state[3][row]
             
             if direction == 1: #clockwise
                 if row == 0:
-                    self.state[0] = [list(x) for x in zip(*reversed(self.state[0]))] #Transpose top
+                    new_state[0] = [list(x) for x in zip(*reversed(self.state[0]))][::-1] #Transpose top
                 elif row == len(self.state[0]) - 1:
-                    self.state[5] = [list(x) for x in zip(*reversed(self.state[5]))] #Transpose bottom
+                    new_state[5] = [list(x) for x in zip(*reversed(self.state[5]))][::-1] #Transpose bottom
             else: #counterclockwise
                 if row == 0:
-                    self.state[0] = [list(x) for x in zip(*self.state[0])][::-1] #Transpose top
+                    new_state[0] = [list(x) for x in zip(*self.state[0])][::-1]
                 elif row == len(self.state[0]) - 1:
-                    self.state[5] = [list(x) for x in zip(*self.state[5])][::-1] #Transpose bottom
+                    new_state[5] = [list(x) for x in zip(*self.state[5])][::-1]
+            self.state = new_state.copy()
         except:
             print("Invalid row number")
 
@@ -63,23 +65,24 @@ class Cube:
             column: int, the column to twist
             direction: int, 1 for clockwise, -1 for counterclockwise
         """
+        new_state = np.copy(self.state)
         try:
             assert column < self.size
             if direction == 1: # clockwise
-                self.state[0][:,column], self.state[2][:,column], self.state[5][:,column], self.state[4][:,column] = self.state[4][:,column],self.state[0][:,column],self.state[2][:,column],self.state[5][:,column]
+                new_state[0][:, column], new_state[2][:, column], new_state[5][:, column], new_state[4][:, column] = self.state[4][:, column],self.state[0][:, column],self.state[2][:, column],self.state[5][:, column]
             else: # counterclockwise
-                self.state[0][:,column], self.state[2][:,column], self.state[5][:,column], self.state[4][:,column] = self.state[2][:,column],self.state[5][:,column],self.state[4][:,column],self.state[0][:,column]
-            
+                new_state[0][:, column], new_state[2][:, column], new_state[5][:, column], new_state[4][:, column] = self.state[2][:, column],self.state[5][:, column],self.state[4][:, column],self.state[0][:, column]
             if direction == 1: #clockwise
                 if column == 0:
-                    self.state[1] = [list(x) for x in zip(*reversed(self.state[1]))][::-1] #Transpose left
+                    new_state[1] = [list(x) for x in zip(*reversed(self.state[1]))][::-1] #Transpose left
                 elif column == len(self.state[1]) - 1:
-                    self.state[3] = [list(x) for x in zip(*reversed(self.state[3]))] #Transpose right
+                    new_state[3] = [list(x) for x in zip(*reversed(self.state[3]))][::-1] #Transpose right
             else: #counterclockwise
                 if column == 0:
-                    self.state[1] = [list(x) for x in zip(*self.state[1])][::-1] #Transpose left
+                    new_state[1] = [list(x) for x in zip(*self.state[1])][::-1]
                 elif column == len(self.state[1]) - 1:
-                    self.state[3] = [list(x) for x in zip(*self.state[3])] #Transpose right
+                    new_state[3] = [list(x) for x in zip(*self.state[3])][::-1]
+            self.state = new_state.copy()
         except:
             print("Invalid column number")
 
@@ -89,37 +92,71 @@ class Cube:
             face: int, the face to rotate
             direction: int, 1 for clockwise, -1 for counterclockwise
         """
+        new_state = np.copy(self.state)
         if direction == 1:
-            self.state[face] = [list(x) for x in zip(*reversed(self.state[face]))][::-1]
+            new_state[face] = [list(x) for x in zip(*reversed(self.state[face]))][::-1]
         else:
-            self.state[face] = [list(x) for x in zip(*self.state[face])][::-1]
+            new_state[face] = [list(x) for x in zip(*self.state[face])][::-1]
+        self.state = new_state.copy()
 
 
-    def show_cube(self):
+    def show_cube_2D(self):
         """Show the current state of the cube, with color, for visualization."""
-        #Build the 3D cube with the right colors
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        
-        # Define the vertices of the cube
-        r = [-1, 1]
-        for s, e in combinations(np.array(list(combinations(r, 3))), 2):
-            if np.sum(np.abs(s-e)) == r[1]-r[0]:
-                ax.plot3D(*zip(s, e), color="b")
-
-        # Define the colors of the faces
-        colors = [self.color_dict[i] for i in range(6)]
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8))
         for i in range(6):
-            ax.add_collection3d(Poly3DCollection([list(zip(*self.state[i]))], color=colors[i], linewidths=1, edgecolors='black', alpha=0.5))
-
-        # Set the viewing angle
-        ax.view_init(30, 30)
+            ax = axs[i//3, i%3]
+            ax.imshow(self.state[i])
+            ax.set_title(self.face_name_dict[i])
+            ax.axis('off')
+            for j in range(self.size):
+                for k in range(self.size):
+                    label = str(self.state[i][j][k])
+                    ax.text(k, j, label, ha='center', va='center', fontsize=12, color='black')
+                    ax.add_patch(matplotlib.patches.Rectangle((k-0.5, j-0.5), 1, 1, edgecolor='black', linewidth=1, facecolor=self.color_dict[self.state[i][j][k]]))
         plt.show()
 
+    def get_face_3D_position(self,i,j,k):
+        if i == 0 : #top
+            return (j,k,self.size), (j,k+1,self.size), (j+1,k+1,self.size), (j,k+1,self.size)
+        elif i == 5: #bottom
+            return (j,k,0), (j+1,k,0), (j+1,k+1,0), (j,k+1,0)
+        elif i == 1: #left
+            return (0,j,k), (0,j+1,k), (0,j+1,k+1), (0,j,k+1)
+        elif i == 3: #right
+            return (self.size,j,k), (self.size,j+1,k), (self.size,j+1,k+1), (self.size,j,k+1)
+        elif i == 2: #front
+            return (j,0,k), (j+1,0,k), (j+1,0,k+1), (j,0,k+1)
+        elif i == 4: #back 
+            return (j,self.size,k), (j+1,self.size,k), (j+1,self.size,k+1), (j,self.size,k+1)
 
+    def show_cube_3D(self):
+        """Show the current state of the cube, with color and borders, for visualization."""
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
+        # Draw faces
+        for i in range(6):
+            for j in range(self.size):
+                for k in range(self.size):
+                    v1, v2, v3, v4 = self.get_face_3D_position(i, j, k)
+                    ax.add_collection3d(Poly3DCollection([[v1, v2, v3, v4]], color=self.color_dict[self.state[i][j][k]]))
 
+        # Draw edges
+        for i in range(6):
+            for j in range(self.size):
+                for k in range(self.size):
+                    v1, v2, v3, v4 = self.get_face_3D_position(i, j, k)
+                    edges = [ [v1, v2], [v2, v3], [v3, v4], [v4, v1] ]
+                    ax.add_collection3d(Line3DCollection(edges, color='black', linewidths=1, linestyles='solid'))
 
-       
+        ax.set_xlim([0, self.size])
+        ax.set_ylim([0, self.size])
+        ax.set_zlim([0, self.size])
+        ax.set_axis_off()
+        ax.set_aspect('equal', adjustable='box')       
+        plt.show()
+        
 cube_variable = Cube(3)
-cube_variable.show_cube()
+cube_variable.show_cube_3D()
+cube_variable.horizontal_twist(1,1)
+cube_variable.show_cube_3D()
