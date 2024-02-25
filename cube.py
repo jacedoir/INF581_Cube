951ce99a2bf0c7844d18a526d7805a043bbab8e3
Ka0rX <guillaume.martinf@gmail.com>
2024-02-25 11:39:05 +0100

    PROOOOD

--- cube.py
+++ cube.py
@@ -10,12 +10,15 @@ from tqdm.notebook import tqdm
 # - https://gymnasium.farama.org/api/env/
 # - https://gymnasium.farama.org/tutorials/gymnasium_basics/environment_creation/#sphx-glr-tutorials-gymnasium-basics-environment-creation-py
 
+
 class CubeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
     def __init__(self, render_mode=None):
-        self.size = 2 # size of rubik's cube
-        self.state = np.chararray((6, self.size, self.size), unicode=True) # initialize cube config
+        self.size = 2  # size of rubik's cube
+        self.state = np.chararray(
+            (6, self.size, self.size), unicode=True
+        )  # initialize cube config
 
         # a chaque fois qu'on rajoute une dim, on rajoute 6 coups possible (2 par nouveau plan, x3 car nb d'axes de l'espace)
         self.action_space = spaces.Discrete(2 * 3 * (self.size - 1))
@@ -24,15 +27,30 @@ class CubeEnv(gym.Env):
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
-        self._position_dict = {0: "left", 1:"top", 2:"front", 3:"bottom", 4:"back", 5:"right"}
-        self._color_dict = {"r": "red", "g": "green", "b": "blue", "o": "orange", "y": "yellow", "w": "black", "":"none"}
-        self._action_map = { # !!!!!! WARNING !!!!!! CHANGE WITH SIZE
+        self._position_dict = {
+            0: "left",
+            1: "top",
+            2: "front",
+            3: "bottom",
+            4: "back",
+            5: "right",
+        }
+        self._color_dict = {
+            "r": "red",
+            "g": "green",
+            "b": "blue",
+            "o": "orange",
+            "y": "yellow",
+            "w": "black",
+            "": "none",
+        }
+        self._action_map = {  # !!!!!! WARNING !!!!!! CHANGE WITH SIZE
             0: lambda: self._horizontale_rotation(0, 1),
             1: lambda: self._horizontale_rotation(0, -1),
             2: lambda: self._verticale_rotation(0, 1),
             3: lambda: self._verticale_rotation(0, -1),
             4: lambda: self._face_rotation(0, 1),
-            5: lambda: self._face_rotation(0, -1)
+            5: lambda: self._face_rotation(0, -1),
         }
 
         self.window_size = 512  # The size of the PyGame window
@@ -41,7 +59,7 @@ class CubeEnv(gym.Env):
 
     def _get_obs(self):
         return self.state
-    
+
     def _get_info(self):
         """Fully known environment"""
         return {}
@@ -51,15 +69,16 @@ class CubeEnv(gym.Env):
             pygame.display.quit()
             pygame.quit()
 
-    def reset(self, seed=None, options=None):
+    def reset(self, n_move, seed=None, options=None):
         """Puts Rubik's cube back to fully solved"""
-        self.state[0] = np.array([["o"]*self.size]*self.size)
-        self.state[1] = np.array([["w"]*self.size]*self.size)
-        self.state[2] = np.array([["g"]*self.size]*self.size)
-        self.state[3] = np.array([["y"]*self.size]*self.size)
-        self.state[4] = np.array([["r"]*self.size]*self.size)
-        self.state[5] = np.array([["b"]*self.size]*self.size)
+        self.state[0] = np.array([["o"] * self.size] * self.size)
+        self.state[1] = np.array([["w"] * self.size] * self.size)
+        self.state[2] = np.array([["g"] * self.size] * self.size)
+        self.state[3] = np.array([["y"] * self.size] * self.size)
+        self.state[4] = np.array([["r"] * self.size] * self.size)
+        self.state[5] = np.array([["b"] * self.size] * self.size)
 
+        self.shuffle(n_move)
         observation = self._get_obs()
         info = self._get_info()
 
@@ -84,7 +103,7 @@ class CubeEnv(gym.Env):
             self._render_frame()
 
         return observation, reward, terminated, False, info
-    
+
     def reward(self, state):
         # TODO
         return int(self._is_solved(state))
@@ -93,16 +112,14 @@ class CubeEnv(gym.Env):
         """Renders one frame"""
         if self.render_mode == "rgb_array":
             return self._render_frame()
-    
+
     def _render_frame(self):
 
         # init window
         if self.window is None and self.render_mode == "human":
             pygame.init()
             pygame.display.init()
-            self.window = pygame.display.set_mode(
-                (self.window_size, self.window_size)
-            )
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
 
@@ -156,87 +173,95 @@ class CubeEnv(gym.Env):
 
     def _is_solved(self, state):
         for i in range(6):
-            if len(np.unique(state[i])) != 1: # check for all face uniqueness
+            if len(np.unique(state[i])) != 1:  # check for all face uniqueness
                 return False
         return True
 
     def _horizontale_rotation(self, row, direction):
         new_state = np.copy(self.state)
-        if row > self.size-1:
-            raise ValueError("Row number must be between 0 and ", self.size-1)
-        if direction == 1: #To the right
+        if row > self.size - 1:
+            raise ValueError("Row number must be between 0 and ", self.size - 1)
+        if direction == 1:  # To the right
             new_state[2, row, :] = self.state[0, row, :]
             new_state[5, row, :] = self.state[2, row, :]
             new_state[4, row, :] = self.state[5, row, :]
             new_state[0, row, :] = self.state[4, row, :]
             if row == 0:
                 new_state[1] = np.rot90(self.state[1])
-            elif row == self.size-1:
-                new_state[3] = np.rot90(self.state[3],3)
-        elif direction == -1: #To the left
+            elif row == self.size - 1:
+                new_state[3] = np.rot90(self.state[3], 3)
+        elif direction == -1:  # To the left
             new_state[0, row, :] = self.state[2, row, :]
             new_state[2, row, :] = self.state[5, row, :]
             new_state[5, row, :] = self.state[4, row, :]
             new_state[4, row, :] = self.state[0, row, :]
             if row == 0:
-                new_state[1] = np.rot90(self.state[1],3)
-            elif row == self.size-1:
+                new_state[1] = np.rot90(self.state[1], 3)
+            elif row == self.size - 1:
                 new_state[3] = np.rot90(self.state[3])
         self.state = new_state
 
     def _verticale_rotation(self, column, direction):
         new_state = np.copy(self.state)
-        if column > self.size-1:
-            raise ValueError("Column number must be between 0 and ", self.size-1)
-        if direction == 1: #to the top
+        if column > self.size - 1:
+            raise ValueError("Column number must be between 0 and ", self.size - 1)
+        if direction == 1:  # to the top
             new_state[2, :, column] = self.state[3, :, column]
             new_state[3, :, column] = self.state[4, :, column]
             new_state[4, :, column] = self.state[1, :, column]
             new_state[1, :, column] = self.state[2, :, column]
             if column == 0:
                 new_state[0] = np.rot90(self.state[0])
-            elif column == self.size-1:
-                new_state[5] = np.rot90(self.state[5],3)
-        elif direction == -1: #to the bottom
+            elif column == self.size - 1:
+                new_state[5] = np.rot90(self.state[5], 3)
+        elif direction == -1:  # to the bottom
             new_state[3, :, column] = self.state[2, :, column]
             new_state[4, :, column] = self.state[3, :, column]
             new_state[1, :, column] = self.state[4, :, column]
             new_state[2, :, column] = self.state[1, :, column]
             if column == 0:
-                new_state[0] = np.rot90(self.state[0],3)
-            elif column == self.size-1:
+                new_state[0] = np.rot90(self.state[0], 3)
+            elif column == self.size - 1:
                 new_state[5] = np.rot90(self.state[5])
         self.state = new_state
-    
+
     def _face_rotation(self, face, direction):
         new_state = np.copy(self.state)
-        if face > self.size-1:
-            raise ValueError("Face number must be between 0 and ", self.size-1)
-        if direction == 1: #clockwise
-            new_state[1,self.size-1-face,:] = self.state[0,:,self.size-1-face]
-            new_state[5, :, face] = self.state[1,self.size-1-face,:]
-            new_state[3,face,:] = self.state[5,:,self.size-1-face]
-            new_state[0,:,self.size-1-face] = self.state[3,face,:]
+        if face > self.size - 1:
+            raise ValueError("Face number must be between 0 and ", self.size - 1)
+        if direction == 1:  # clockwise
+            new_state[1, self.size - 1 - face, :] = self.state[
+                0, :, self.size - 1 - face
+            ]
+            new_state[5, :, face] = self.state[1, self.size - 1 - face, :]
+            new_state[3, face, :] = self.state[5, :, self.size - 1 - face]
+            new_state[0, :, self.size - 1 - face] = self.state[3, face, :]
             if face == 0:
-                new_state[2] = np.rot90(self.state[2],3)
-            elif face == self.size-1:
+                new_state[2] = np.rot90(self.state[2], 3)
+            elif face == self.size - 1:
                 new_state[4] = np.rot90(self.state[4])
-        elif direction == -1: #counterclockwise
-            new_state[0,:,self.size-1-face] = self.state[1,self.size-1-face,:]
-            new_state[1,self.size-1-face,:] = self.state[5, :, self.size-1-face]
-            new_state[5, :, face] = self.state[3,face,:]
-            new_state[3,face,:] = self.state[0,:,self.size-1-face]
+        elif direction == -1:  # counterclockwise
+            new_state[0, :, self.size - 1 - face] = self.state[
+                1, self.size - 1 - face, :
+            ]
+            new_state[1, self.size - 1 - face, :] = self.state[
+                5, :, self.size - 1 - face
+            ]
+            new_state[5, :, face] = self.state[3, face, :]
+            new_state[3, face, :] = self.state[0, :, self.size - 1 - face]
             if face == 0:
                 new_state[2] = np.rot90(self.state[2])
-            elif face == self.size-1:
-                new_state[4] = np.rot90(self.state[4],3)
+            elif face == self.size - 1:
+                new_state[4] = np.rot90(self.state[4], 3)
         self.state = new_state
 
     def shuffle(self, n_moves):
         # TODO (not important) able to use seed
+        observations = []
+        infos = []
         for i in range(n_moves):
             face = np.random.randint(self.size)
-            direction = np.random.choice([-1,1])
+            direction = np.random.choice([-1, 1])
             operation = np.random.randint(3)
             if operation == 0:
                 self.horizontale_rotation(face, direction)
